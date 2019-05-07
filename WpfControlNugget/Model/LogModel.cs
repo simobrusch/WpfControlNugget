3182e6566bbc7802d65d0bd6f27985389133640d
Simo Brusch <simo.brusch@bluewin.ch>
2019-05-07 17:07:26 +0200

    mvvm ordner und klassen

---
+++ WpfControlNugget/Model/LogModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlNugget.Model
+{
+    public class LogModel
+    {
+        public int idLogging { get; set; }
+        public string pod { get; set; }
+        public string location { get; set; }
+        public string hostname { get; set; }
+        public string severity { get; set; }
+        public DateTime zeit { get; set; }
+        public string message { get; set; }
+    }
+}
