e8a65501f3ba59f880816ac8d9ce0abe08f43787
Simo Brusch <simo.brusch@bluewin.ch>
2019-05-21 13:31:32 +0200

    added class duplicatechecker and Interface IEntity

---
+++ WpfControlNugget/ViewModel/DuplicatesChecker/DuplicateChecker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlNugget.ViewModel.DuplicatesChecker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace WpfControlNugget.ViewModel.DuplicateChecker
+    {
+        public class DuplicateChecker
+        {
+            public IEnumerable<IEntity> FindDuplicates(IEnumerable<IEntity> list)
+            {
+                var hashSet = new HashSet<IEntity>();
+                var ret = new List<IEntity>();
+                foreach (var item in list)
+                {
+                    if (!hashSet.Add(item))
+                    {
+                        ret.Add(item);
+                    }
+                }
+
+                return ret;
+            }
+        }
+    }
+}
---
+++ WpfControlNugget/ViewModel/DuplicatesChecker/IEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlNugget.ViewModel.DuplicatesChecker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace WpfControlNugget.ViewModel.DuplicateChecker
+    {
+        public interface IEntity
+        {
+        }
+    }
+}
