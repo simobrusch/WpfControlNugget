27a157a76062610c409a73d52c8da6d5e3db338d
Simo Brusch <simo.brusch@bluewin.ch>
2019-06-11 18:09:45 +0200

    implemented methods for logmodelrepository

--- WpfControlNugget/Repository/LogModelRepository.cs
+++ WpfControlNugget/Repository/LogModelRepository.cs
@@ -14,6 +14,7 @@ namespace WpfControlNugget.Repository
     class LogModelRepository : RepositoryBase<LogModel>
     {
         public List<LogModel> Logs { get; set; }
+        public LogModel _Logs { get; set; }
 
         public LogModelRepository(string connectionString) : base(connectionString)
         {
@@ -21,7 +22,35 @@ namespace WpfControlNugget.Repository
 
         public override LogModel GetSingle<P>(P pkValue)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var conn = new MySqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    using (var cmd = new MySqlCommand("SELECT id, pod, location, hostname, severity, timestamp, message FROM " + TableName + "WHERE id =" + pkValue, conn))
+                    {
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            _Logs = (new LogModel(
+
+                                reader.GetInt32("id"),
+                                reader.GetValue(reader.GetOrdinal("pod")) as string,
+                                reader.GetValue(reader.GetOrdinal("location")) as string,
+                                reader.GetValue(reader.GetOrdinal("hostname")) as string,
+                                reader.GetString("severity"),
+                                reader.GetDateTime("timestamp"),
+                                reader.GetValue(reader.GetOrdinal("message")) as string
+                            ));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred: " + ex.Message);
+            }
+            return _Logs;
         }
         
 
@@ -42,7 +71,43 @@ namespace WpfControlNugget.Repository
 
         public override List<LogModel> GetAll(string whereCondition, Dictionary<string, object> parameterValues)
         {
-            throw new NotImplementedException();
+            var whereCon = whereCondition;
+            if (parameterValues.Count > 0 && whereCondition != null)
+            {
+                foreach (KeyValuePair<string, object> p in parameterValues)
+                {
+                    whereCon = whereCon.Replace($"@{p.Key}", p.Value.ToString());
+                }
+            }
+            try
+            {
+                using (var conn = new MySqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    using (var cmd = new MySqlCommand("SELECT id, pod, location, hostname, severity, timestamp, message FROM " + TableName + "WHERE " + whereCon, conn))
+                    {
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Logs.Add(new LogModel(
+
+                                reader.GetInt32("id"),
+                                reader.GetValue(reader.GetOrdinal("pod")) as string,
+                                reader.GetValue(reader.GetOrdinal("location")) as string,
+                                reader.GetValue(reader.GetOrdinal("hostname")) as string,
+                                reader.GetString("severity"),
+                                reader.GetDateTime("timestamp"),
+                                reader.GetValue(reader.GetOrdinal("message")) as string
+                            ));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred: " + ex.Message);
+            }
+            return Logs;
         }
 
         public override List<LogModel> GetAll()
@@ -52,7 +117,7 @@ namespace WpfControlNugget.Repository
                 using (var conn = new MySqlConnection(ConnectionString))
                 {
                     conn.Open();
-                    using (var cmd = new MySqlCommand("SELECT id, pod, location, hostname, severity, timestamp, message FROM v_logentries ORDER BY timestamp", conn))
+                    using (var cmd = new MySqlCommand("SELECT id, pod, location, hostname, severity, timestamp, message FROM" + TableName + "ORDER BY timestamp", conn))
                     {
                         var reader = cmd.ExecuteReader();
                         while (reader.Read())
